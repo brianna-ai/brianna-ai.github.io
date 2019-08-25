a556f5589179773df352b60af406d2cfccf01eca
Brianna <brianna.ying@gmail.com>
2019-08-25 01:36:17 -0600

    Added hide function in scripts

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,35 +1,38 @@
-function hide () {
+var elemId = null;
+
+function hide (id) {
   var elem = document.getElementById("active");
-  if (elem != null) {
+
+  if (elem == null) {
+    elemId = id;
+  } else if (elem != null) {
     elem.style.display = "none";
-    elem.id="fourth";
+    elem.id = elemId;
+    elemId = id;
   }
+
 }
 
 function show(id) {
   if (id == "firsttab") {
-    hide();
+    hide("first");
+    document.getElementById("first").style.display = "block"
     document.getElementById("first").id = "active";
 
-    //document.getElementById("first").style.display = "block";
-    //document.getElementById("fourth").style.display = "none";
   } else if (id == "secondtab") {
-    hide();
+    hide("second");
+    document.getElementById("second").style.display = "block"
     document.getElementById("second").id = "active";
 
-    //document.getElementById("second").style.display = "block";
-    //document.getElementById("third").style.display = "none";
   } else if (id == "thirdtab") {
-    hide();
+    hide("third");
+    document.getElementById("third").style.display = "block"
     document.getElementById("third").id = "active";
 
-    //document.getElementById("third").style.display = "block";
-    //document.getElementById("fourth").style.display = "none";
   } else if (id == "fourthtab") {
-    hide();
+    hide("fourth");
+    document.getElementById("fourth").style.display = "block"
     document.getElementById("fourth").id = "active";
 
-    //document.getElementById("fourth").style.display = "block";
-    //document.getElementById("third").style.display = "none";
   }
 }
