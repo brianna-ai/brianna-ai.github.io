7a2a603215c20fec7e94e78cca0732eedb143cd8
Brianna <brianna.ying@gmail.com>
2019-08-25 22:47:18 -0600

    Added bg and changing border colour (keep?)

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -18,21 +18,25 @@ function show(id) {
     hide("first");
     document.getElementById("first").style.display = "block"
     document.getElementById("first").id = "active";
+    document.getElementById("intro").style.borderColor = "#FF6F61";
 
   } else if (id == "secondtab") {
     hide("second");
     document.getElementById("second").style.display = "block"
     document.getElementById("second").id = "active";
+    document.getElementById("intro").style.borderColor = "#0094D5";
 
   } else if (id == "thirdtab") {
     hide("third");
     document.getElementById("third").style.display = "block"
     document.getElementById("third").id = "active";
+    document.getElementById("intro").style.borderColor = "#FEDD00";
 
   } else if (id == "fourthtab") {
     hide("fourth");
     document.getElementById("fourth").style.display = "block"
     document.getElementById("fourth").id = "active";
+    document.getElementById("intro").style.borderColor = "#00939D";
 
   }
 }
